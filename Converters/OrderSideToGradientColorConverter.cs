642efe3d872dff21e548c490609c0f22c850b3fa
AlexKuro <alexkuro182@gmail.com>
2020-06-15 15:49:07 +0300

    Tradehistory

---
+++ Converters/OrderSideToGradientColorConverter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace TradingHistory.Converters
+{
+    public class OrderSideToGradientColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = (bool)(value);
+
+            var buying = (LinearGradientBrush)Application.Current.Resources["TradingHistoryBuyGradient"];
+            var selling = (LinearGradientBrush)Application.Current.Resources["TradingHistorySellGradient"];
+            
+
+            switch (val)
+            {
+                case true:
+                    return selling;
+                case false:
+                    return buying;
+                default:
+                    return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {         
+            return null;
+        }
+    }
+    public class OrderSideToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = (bool)(value);
+
+            var buying = (SolidColorBrush)Application.Current.Resources["CommonGreenColor"];
+            var selling = (SolidColorBrush)Application.Current.Resources["CommonPinkColor"];
+           
+
+            switch (val)
+            {
+                case true:
+                    return selling;
+                case false:
+                    return buying;
+                default:
+                    return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
